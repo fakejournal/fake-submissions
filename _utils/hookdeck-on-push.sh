e41af5d4465fe85c0be283ba5cc0305bc126f509
Neruthes <i@neruthes.xyz>
2026-07-06 19:08:18 +0000

    Regular update '2026-07-06T19:08:18+00:00'

--- _utils/hookdeck-on-push.sh
+++ _utils/hookdeck-on-push.sh
@@ -3,7 +3,19 @@
 date -Is > /tmp/hookdeck-on-push.sh.txt
 
 
-git pull &&
+retry() {
+    local max=$1
+    shift
+
+    local i
+    for ((i=1; i<=max; i++)); do
+        "$@" && return 0
+        (( i == max )) && return 1
+        sleep 5
+    done
+}
+
+retry 10 git pull &&
 ./make.sh wwwsrc/ &&
 bash wwwsrc/cfwsdeploy.sh
 
